d8977f72cb1d0a69907977e3a4401e66c24676e7
edeng12345 <146144686+edeng12345@users.noreply.github.com>
2023-10-23 03:01:04 +0000

    square

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
